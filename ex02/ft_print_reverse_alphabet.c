2c20565a835c971f1cf2eb2ae5302ba9187055d5
Issaka hama barhamou <hamabarhamou@gmail.com>
2021-09-08 12:53:53 +0100

    Ajout de exo 2

--- ex02/ft_print_reverse_alphabet.c
+++ ex02/ft_print_reverse_alphabet.c
@@ -1,25 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bissaka- <bissaka-@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/07 16:40:51 by bissaka-          #+#    #+#             */
-/*   Updated: 2021/09/08 12:46:14 by bissaka-         ###   ########.fr       */
+/*   Created: 2021/09/08 12:38:55 by bissaka-          #+#    #+#             */
+/*   Updated: 2021/09/08 12:49:20 by bissaka-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<unistd.h>
 
-void	ft_print_alphabet(void)
+void	ft_print_reverse_alphabet(void)
 {
 	char	car;
 
-	car = 'a';
-	while (car <= 'z')
+	car = 'z';
+	while (car >= 'a')
 	{
 		write(1, &car, 1);
-		car += 1;
+		car -= 1;
 	}
 }
